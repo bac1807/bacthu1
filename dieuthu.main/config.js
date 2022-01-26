00c23108b6b7860d9eb1d813f38c0d98ecbd6abc
bac1807 <65804384+bac1807@users.noreply.github.com>
2022-01-26 14:19:33 +0700

    Add files via upload

---
+++ dieuthu.main/config.js
@@ -0,0 +1,26 @@
+const CONFIG = {
+    introTitle: ' Công chúa của anh ',
+    introDesc: ` 
+        Thư à anh cảm thấy mình rất là may mắn khi được làm Người em yêu
+        anh thì không phải người giỏi hiểu con gái tụi em nhưng anh yêu em 
+        rất trân thành anh không dám hứa hay thề gì cả nhưng anh sẽ cố gắng làm cho em
+        hạnh phúc nhất có thể và không làm cho em buồn anh văn thơ anh hơi dốt
+        nhưng Yêu em thì nó là real em hãy bên anh thật lâu nhé
+
+              
+                  `,
+          
+
+    btnIntro: 'Dạ',
+    title: 'Bắc Yêu Thư',
+    desc: 'Thư có yêu Bắc khumm Thư muốn ở cùng Bắc Mãi mãi khumm',
+    btnYes: 'Có ạ <3',
+    btnNo: 'Không :((',
+    question:'Trên thế giới hơn 7 tỉ người mà em là người đẹp nhất <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Yêu thương công chúa của anh.',
+    btnAccept: 'Thương',
+    messLink: 'https://www.facebook.com/nguyenphuong.bac.585/' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
